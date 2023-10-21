2cc0a565fa759f77b75dc690717cbee918a9e3cb
Kavindu <kavindupriyanath@gmail.com>
2023-10-21 23:25:46 +0530

    Player data from game is saving in cloud

--- user.js
+++ user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { loginUser } = require("./login_logic");
+const { saveProgress } = require("./PlayerProgress");
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/login", loginUser);
+
+router.get("/playersave", saveProgress);
 
 module.exports = router;
